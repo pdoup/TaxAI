1c57c38ac558323382592d6880b84203efd7f278
pdoup <doupanab@gmail.com>
2025-05-15 20:46:01 +0300

    add JWT

--- tax-filer-backend/app/core/config.py
+++ tax-filer-backend/app/core/config.py
@@ -34,7 +34,7 @@ class Settings(BaseSettings):
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(
         os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
-    )  # Token valid for 30 minutes
+    )  # Token valid for 30 minutes (default)
 
     model_config = ConfigDict(
         extra="allow", env_file=".env", env_file_encoding="utf-8", case_sensitive=True
